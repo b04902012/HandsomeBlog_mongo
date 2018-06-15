5051c024e4dd3a14dacff5e4ad5765ca2057ca23
albert0liu0 <albert0liu0@gmail.com>
2018-06-15 16:36:35 +0800

    mongo

--- homepage.js
+++ homepage.js
@@ -1,9 +1,7 @@
-var select=require('./dbcommand').select
 var url=require('url')
 var fs=require('fs')
-module.exports=function(pool,req,res,usr){
+module.exports=function(req,res){
     console.log('homepage')
     res.writeHead(200,{'Content-Type':'text/html'})
     fs.createReadStream('./data/home.html').pipe(res)
 }
-
--- pagemodule.js
+++ pagemodule.js
@@ -1,9 +1,3 @@
 module.exports={
     '/':require('./homepage'),
-    '/login':require('./login'),
-    '/register':require('./register'),
-    '/cookietest':require('./cookietest'),
-    '/editpage':require('./editpage'),
-    //'/getpage':require( ),
-    '/checkusername':require('./register').checkusername
 }
