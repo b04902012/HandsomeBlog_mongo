827cb7a4c84edcbf9a067fe8a2da12f83a615d8b
albert0liu0 <albert0liu0@gmail.com>
2018-06-15 16:23:18 +0800

    userModule

--- data/login.js
+++ data/login.js
@@ -1,3 +1,4 @@
+import userModule from '../userModule.js'
 window.addEventListener('load',function(event){
     var username=document.getElementById('username')
     var password=document.getElementById('password')
@@ -9,71 +10,7 @@ window.addEventListener('load',function(event){
             username:username.value,
             password:password.value,
         }
-        login_request.open(
-            'GET',
-            './login?'+encodeURIComponent(JSON.stringify(data))
-        )
-        login_request.send()
-        login_request.onreadystatechange=function(){
-            if(login_request.readyState==3)
-                status_login.innerHTML='Loading<br>'
-            if(login_request.readyState==4){
-                if(login_request.status=='200'){
-                    if(login_request.responseText=='0'){
-                        status_login.style.color='green'
-                        status_login.innerHTML='Succefully logged in.<br>'
-                    }
-                    else{
-                        status_login.style.color='red'
-                        status_login.innerHTML='Incorrect username or password.<br>'
-                    }
-                }
-                else{
-                    status_login.style.color='red'
-                    status_login.innerHTML='An error occurs, please try again.<br>'
-                }
-            }
-		}
-    })
-})
-
-
-
-window.addEventListener('load',function(event){
-    var usrn=document.getElementById('usrn')
-    var pwd=document.getElementById('pwd')
-    var lgn=document.getElementById('lgn')
-    var stt_lgn=document.getElementById('stt_lgn')
-    lgn.addEventListener('click',function(event){
-        var req=new XMLHttpRequest()
-        var data={
-            username:usrn.value,
-            password:pwd.value,
-        }
-        req.open(
-            'GET',
-            './login?'+encodeURIComponent(JSON.stringify(data))
-        )
-        req.send()
-        req.onreadystatechange=function(){
-            if(req.readyState==3)
-                stt_lgn.innerHTML='Loading<br>'
-            if(req.readyState==4){
-                if(req.status=='200'){
-                    if(req.responseText=='0'){
-                        stt_lgn.style.color='green'
-                        stt_lgn.innerHTML='Succefully logged in.<br>'
-                    }
-                    else{
-                        stt_lgn.style.color='red'
-                        stt_lgn.innerHTML='Incorrect username or password.<br>'
-                    }
-                }
-                else{
-                    stt_lgn.style.color='red'
-                    stt_lgn.innerHTML='An error occurs, please try again.<br>'
-                }
-            }
-        }
+        userModule.logIn(username,password)
+        
     })
 })
